6674c95432f5569988ac7514e4e0473829312836
Jamie Meredith <jlmeredith@gmail.com>
2021-06-13 18:55:43 -0600

    Create my_initials.py
    I recently completed the block exercise for lesson one of CS101.  After looking at the hints I realized that I had over-abstracted the lesson a bit.  After posting to the Discord group, someone mentioned I should submit a pull request, so here I am, lol.

---
+++ my_initials.py
@@ -0,0 +1,63 @@
+# Jamie M
+# I like icecream
+
+# Build the letter J 
+j_line1 = "JJJJJ"
+j_line2 = "  J  "
+j_line3 = "  J  "
+j_line4 = "  J  "
+j_line5 = "J J  "
+j_line6 = "J J  "
+j_line7 = " JJ  "
+
+# Concatenate the letter J into a single variable
+j_letter = j_line1 + "\n" + j_line2 + "\n" + j_line3 + "\n" + j_line4 + "\n" + j_line5 + "\n" + j_line6 + "\n" + j_line7 
+
+# Build the letter A 
+a_line1 = "  A  "
+a_line2 = " A A "
+a_line3 = "A   A"
+a_line4 = "A   A"
+a_line5 = "AAAAA"
+a_line6 = "A   A"
+a_line7 = "A   A"
+
+# Concatenate the letter A into a single variable
+a_letter = a_line1 + "\n" + a_line2 + "\n" + a_line3 + "\n" + a_line4 + "\n" + a_line5 + "\n" + a_line6 + "\n" + a_line7 
+
+# Build the letter M 
+m_line1 = "M   M"
+m_line2 = "MM MM"
+m_line3 = "MM MM"
+m_line4 = "M M M"
+m_line5 = "M   M"
+m_line6 = "M   M"
+m_line7 = "M   M"
+# Concatenate the letter M into a single variable
+m_letter = m_line1 + "\n" + m_line2 + "\n" + m_line3 + "\n" + m_line4 + "\n" + m_line5 + "\n" + m_line6 + "\n" + m_line7 
+
+# Build the letter I 
+i_line1 = "IIIII"
+i_line2 = "  I  "
+i_line3 = "  I  "
+i_line4 = "  I  "
+i_line5 = "  I  "
+i_line6 = "  I  "
+i_line7 = "IIIII"
+
+# Concatenate the letter I into a single variable
+i_letter = i_line1 + "\n" + i_line2 + "\n" + i_line3 + "\n" + i_line4 + "\n" + i_line5 + "\n" + i_line6 + "\n" + i_line7 
+
+# Build the letter E
+e_line1 = "EEEEE"
+e_line2 = "E    "
+e_line3 = "E    "
+e_line4 = "EEE  "
+e_line5 = "E    "
+e_line6 = "E    "
+e_line7 = "EEEEE"
+# Concatenate the letter E into a single variable
+e_letter = e_line1 + "\n" + e_line2 + "\n" + e_line3 + "\n" + e_line4 + "\n" + e_line5 + "\n" + e_line6 + "\n" + e_line7 
+
+# Print the full name
+print (j_letter + "\n \n" + a_letter + "\n \n" + m_letter + "\n \n" + i_letter + "\n \n" + e_letter)
